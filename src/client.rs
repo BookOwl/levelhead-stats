b70c64bd2ace1857cdc77bba0e6df9b424f1cb6b
BookOwl <stanleybookowl@gmail.com>
2021-04-09 17:58:26 -0400

    Write level data to CSV

--- src/client.rs
+++ src/client.rs
@@ -1,63 +1,61 @@
+use std::collections::{HashMap, HashSet};
 use reqwest;
 use serde_json;
 use reqwest::header::{HeaderName, HeaderValue};
 
-#[derive(Debug)]
 pub struct Level {
-    avatar_id: String,
-    created_at: String,
-    creator_time: f64,
-    cv: u64,
-    is_daily_build: bool,
-    game_version: String,
-    level_id: String,
-    locale: String,
-    locale_id: u64,
-    required_players: u64,
-    stats: Stats,
-    high_score: Vec<Record>,
-    fastest_time: Vec<Record>,
-    tags: Vec<String>,
-    tag_names: Vec<String>,
-    title: String,
-    in_tower: bool,
-    in_tower_trial: bool,
-    updated_at: String,
-    user_id: String,
-    internal_id: String,
+    pub avatar_id: String,
+    pub created_at: String,
+    pub creator_time: f64,
+    pub cv: u64,
+    pub is_daily_build: bool,
+    pub game_version: String,
+    pub level_id: String,
+    pub locale: String,
+    pub locale_id: u64,
+    pub required_players: u64,
+    pub stats: Stats,
+    pub high_scores: Vec<Record>,
+    pub fastest_times: Vec<Record>,
+    pub tags: Vec<String>,
+    pub tag_names: Vec<String>,
+    pub title: String,
+    pub in_tower: bool,
+    pub in_tower_trial: bool,
+    pub updated_at: String,
+    pub user_id: String,
+    pub user_alias: String,
+    pub internal_id: String,
 }
 
-#[derive(Debug)]
 pub struct Stats {
-    attempts: u64,
-    successes: u64,
-    clear_rate: f64,
-    failure_rate: f64,
-    diamonds: u64,
-    likes: u64,
-    favorites: u64,
-    hidden_gem: u64,
-    playtime: f64,
-    players: u64,
+    pub attempts: u64,
+    pub successes: u64,
+    pub clear_rate: f64,
+    pub failure_rate: f64,
+    pub diamonds: u64,
+    pub likes: u64,
+    pub favorites: u64,
+    pub hidden_gem: u64,
+    pub playtime: f64,
+    pub players: u64,
     /// AKA Spice
-    replay_value: f64,
-    time_per_win: f64,
-    exposure_bucks: u64,
+    pub replay_value: f64,
+    pub time_per_win: f64,
+    pub exposure_bucks: u64,
 }
 
-#[derive(Debug)]
 pub enum RecordType {
     HighScore,
     FastestTime,
 }
 
-#[derive(Debug)]
 pub struct Record {
-    record_type: RecordType,
-    value: f64,
-    user_id: String,
-    created_at: String,
-
+    pub record_type: RecordType,
+    pub value: f64,
+    pub user_id: String,
+    pub user_alias: String,
+    pub created_at: String,
 }
 
 pub struct Client {
@@ -75,7 +73,7 @@ impl Client {
         let mut levels = vec![];
         let mut headers = reqwest::header::HeaderMap::new();
         headers.insert(HeaderName::from_static("rumpus-delegation-key"), HeaderValue::from_str(&self.key).unwrap());
-        let mut client = reqwest::Client::builder()
+        let client = reqwest::Client::builder()
             .user_agent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36")
             .default_headers(headers)
             .gzip(true)
@@ -99,7 +97,6 @@ impl Client {
                 .await?;
             let json: serde_json::Value = serde_json::from_str(&response.text().await?).expect("Rumpus should return valid JSON");
             let level_data = json["data"].as_array().expect("Rumpus should return an array");
-            println!("Length of data: {}", level_data.len());
             if level_data.len() == 0 {
                 break
             }
@@ -126,6 +123,7 @@ impl Client {
                     value: high_score["value"].as_f64().unwrap(),
                     created_at: high_score["createdAt"].as_str().unwrap().to_string(),
                     user_id: high_score["userId"].as_str().unwrap().to_string(),
+                    user_alias: String::new(),
                 }).collect::<Vec<_>>();
                 let fastest = &level_obj["records"]["FastestTime"].as_array().unwrap();
                 let fastest = fastest.iter().map(|f| Record {
@@ -133,6 +131,7 @@ impl Client {
                     value: f["value"].as_f64().unwrap(),
                     created_at: f["createdAt"].as_str().unwrap().to_string(),
                     user_id: f["userId"].as_str().unwrap().to_string(),
+                    user_alias: String::new(),
                 }).collect::<Vec<_>>();
                 let level = Level {
                     avatar_id: level_obj["avatarId"].as_str().unwrap().to_string(),
@@ -146,8 +145,8 @@ impl Client {
                     locale_id: level_obj["localeId"].as_u64().unwrap(),
                     required_players: level_obj["requiredPlayers"].as_u64().unwrap(),
                     stats: stats,
-                    high_score: high_scores,
-                    fastest_time: fastest,
+                    high_scores: high_scores,
+                    fastest_times: fastest,
                     tags: level_obj["tags"].as_array().unwrap().iter().map(|d| (&d).as_str().unwrap().to_string()).collect::<Vec<String>>(),
                     tag_names: level_obj["tagNames"].as_array().unwrap().iter().map(|d| (&d).as_str().unwrap().to_string()).collect::<Vec<String>>(),
                     title: level_obj["title"].as_str().unwrap().to_string(),
@@ -155,15 +154,61 @@ impl Client {
                     in_tower_trial: level_obj["towerTrial"].as_bool().unwrap(),
                     updated_at: level_obj["updatedAt"].as_str().unwrap().to_string(),
                     user_id: level_obj["userId"].as_str().unwrap().to_string(),
+                    user_alias: String::new(),
                     internal_id: level_obj["_id"].as_str().unwrap().to_string(),
                 };
-                println!("{}", &level.title);
                 levels.push(level);
             }
             tiebreak_id = Some(levels[levels.len() - 1].internal_id.clone());
             max_created_at = Some(levels[levels.len() - 1].created_at.clone());
             //println!("JSON: {:#?}", &json);
         }
+        let mut user_ids = Vec::with_capacity(levels.len() * 7);
+        for level in &levels {
+            user_ids.push(level.user_id.clone());
+            for record in level.high_scores.iter().chain(level.fastest_times.iter()) {
+                user_ids.push(record.user_id.clone());
+            }
+        }
+        let aliases = self.user_ids_to_aliases(&user_ids).await?;
+        for level in levels.iter_mut() {
+            level.user_alias = aliases.get(&level.user_id).unwrap().clone();
+            for record in level.high_scores.iter_mut() {
+                record.user_alias = aliases.get(&record.user_id).unwrap().clone();
+            }
+            for record in level.fastest_times.iter_mut() {
+                record.user_alias = aliases.get(&record.user_id).unwrap().clone();
+            }
+        }
         Ok(levels)
+    }
+    pub async fn user_ids_to_aliases(&self, user_ids: &[String]) -> reqwest::Result<HashMap<String, String>> {
+        let ids = user_ids.iter().collect::<HashSet<_>>();
+        let ids = ids.iter().collect::<Vec<_>>();
+        let mut aliases: HashMap<String, String> = HashMap::with_capacity(ids.len());
+        let mut headers = reqwest::header::HeaderMap::new();
+        headers.insert(HeaderName::from_static("rumpus-delegation-key"), HeaderValue::from_str(&self.key).unwrap());
+        let client = reqwest::Client::builder()
+            .user_agent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36")
+            .default_headers(headers)
+            .gzip(true)
+            .build()?;
+        for chunk in ids.chunks(64) {
+            let query = [("userIds", chunk.iter().map(|&&s| s.clone()).collect::<Vec<String>>().join(","))];
+            let response = client.get("https://www.bscotch.net/api/levelhead/aliases")
+                .query(&query)
+                .send()
+                .await?;
+            let json: serde_json::Value = serde_json::from_str(&response.text().await?)
+                .expect("Rumpus should return valid JSON");
+            json["data"]
+                .as_array().unwrap()
+                .iter()
+                .map(|d| (d["userId"].as_str().unwrap(), d["alias"].as_str().unwrap()))
+                .for_each(|(id, alias)| {
+                    aliases.insert(id.to_string(), alias.to_string());
+                })
+        }
+        Ok(aliases)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,36 @@
+use std::error::Error;
+
+use text_io::read;
+use csv::Writer;
+
 mod client;
 
 static KEY: &'static str = include_str!("../rumpus_key");
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>>{
+    println!("Enter the levelhead user ID you want to download level statistics for:");
+    let user_id: String = read!();
     let client = client::Client::new(&KEY);
-    let levels = client.levels_by_user("0qvgb6").await?;
-    println!("{:#?}", levels[0]);
+    let levels = client.levels_by_user(&user_id).await?;
+    let mut wrt = Writer::from_path(format!("level_data_{}.csv", user_id)).unwrap();
+    wrt.write_record(&["name", "level_id", "created_at", "in_tower", "in_tower_trial", 
+                        "tags", "high_score", "ribbon_holder", "shoe_time", "shoe_holder",
+                        "required_players", "is_daily_build",
+                        "attempts", "successes", "clear_rate", "failure_rate", "diamonds",
+                        "likes", "favorites", "hidden_gem", "spice", "playtime", "players",
+                        "time_per_win", "exposure_bucks"]).unwrap();
+    for l in levels {
+        wrt.write_record(&[
+            l.title.clone(), l.level_id.clone(), l.created_at.clone(), format!("{:?}", l.in_tower), format!("{:?}", l.in_tower_trial), 
+            l.tag_names.join(";"), format!("{:?}", l.high_scores[0].value), l.high_scores[0].user_alias.clone(), format!("{:?}", l.fastest_times[0].value), l.fastest_times[0].user_alias.clone(), 
+            format!("{:?}", l.required_players), format!("{:?}", l.is_daily_build), 
+            format!("{:?}", l.stats.attempts), format!("{:?}", l.stats.successes), format!("{:?}", l.stats.clear_rate), format!("{:?}", l.stats.failure_rate), format!("{:?}", l.stats.diamonds),
+            format!("{:?}", l.stats.likes), format!("{:?}", l.stats.favorites), format!("{:?}", l.stats.hidden_gem), format!("{:?}", l.stats.replay_value), format!("{:?}", l.stats.playtime), format!("{:?}", l.stats.players),
+            format!("{:?}", l.stats.time_per_win), format!("{:?}", l.stats.exposure_bucks)
+        ]).unwrap();
+    }
+    wrt.flush();
+    //println!("{:#?}", levels[levels.len() - 1]);
     Ok(())
 }
